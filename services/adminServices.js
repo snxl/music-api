a5b2cc2abd934f5d7f0c18ea4c7c6b5218ae541e
Nicolas <nicolas.soares.gomes@gmail.com>
2021-10-06 18:55:28 -0300

    remove transaction sequelize

--- services/adminServices.js
+++ services/adminServices.js
@@ -4,22 +4,18 @@ const { sequelize } = db;
 
 export default class AdminServices {
     static async deleteUser(id) {
-        const t = await sequelize.transaction();
         try {
             await db.User.destroy({
                 where: {
                     id,
                 },
-                transaction: t,
             });
 
-            await t.commit();
             return {
                 status: 'OK',
                 description: 'User deleted successfully',
             };
         } catch (error) {
-            await t.rollback();
             return {
                 status: 'ERR',
                 description: 'Failed to delete user',
@@ -28,7 +24,6 @@ export default class AdminServices {
     }
 
     static async userToProvider(id) {
-        const t = await sequelize.transaction();
         try {
             await db.User.update({
                 provider: true,
@@ -36,16 +31,13 @@ export default class AdminServices {
                 where: {
                     id,
                 },
-                transaction: t,
             });
 
-            await t.commit();
             return {
                 status: 'OK',
                 description: 'User updated successfully',
             };
         } catch (error) {
-            await t.rollback();
             return {
                 status: 'ERR',
                 description: 'Failed to updated user',
@@ -54,7 +46,6 @@ export default class AdminServices {
     }
 
     static async statusUpdate(id, status) {
-        const t = await sequelize.transaction();
         try {
             await db.Song.update({
                 adminStatus: status,
@@ -62,16 +53,13 @@ export default class AdminServices {
                 where: {
                     id,
                 },
-                transaction: t,
             });
 
-            await t.commit();
             return {
                 status: 'OK',
                 description: 'File updated successfully',
             };
         } catch (error) {
-            await t.rollback();
             return {
                 status: 'ERR',
                 description: 'Failed to updated file',
@@ -80,21 +68,17 @@ export default class AdminServices {
     }
 
     static async destroySong(id) {
-        const t = await sequelize.transaction();
         try {
             const destroyFile = await db.Song.destroy({
                 where: {
                     id,
                 },
-                transaction: t,
             });
-            await t.commit();
             return {
                 status: 'OK',
                 description: `lines deleted ${destroyFile}`,
             };
         } catch (error) {
-            await t.rollback();
             return {
                 status: 'ERR',
                 description: 'Failed to destroy file',
